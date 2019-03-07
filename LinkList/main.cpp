1541e88dcd10bac7c16377f3980025dfa87706d2
“牛兴炜” <1669250716@qq.com>
2019-03-07 12:56:04 +0800

    第二次提交

---
+++ LinkList/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+    struct node
+    {
+        int number;
+        struct node *next;
+    };
+int main()
+{
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
